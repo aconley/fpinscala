e1c780cbb8aea02ef33c82b1987d55f2897d3f75
Alex Conley <alexander.conley@colorado.edu>
2014-11-04 17:18:48 -0700

    Do getting started exercises

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -35,8 +35,34 @@ object MyModule {
   }
 
   // Exercise 1: Write a function to compute the nth fibonacci number
+  //  A few solutions presented
 
-  def fib(n: Int): Int = ???
+  // Probably the one they had in mind
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def fibInner(idx: Int, prev: Int, curr: Int): Int = 
+      if (idx <= 0) prev else fibInner(idx - 1, curr, prev + curr)
+    fibInner(n, 0 , 1)
+  }
+
+  // Stream solution, requires 2.11
+  //def fib2(n: Int): Int = {
+  //  require(n >= 1, "Invalid n")
+  //  val str: Stream[Int] = 0 #:: 1 #:: str.zip(str.tail).map(x => x._1 + x._2)
+  //  str(n-1)
+  //}
+  // Stream solution works in 2.8
+  def fib3(n:Int): Int = {
+    def fibFrom(a: Int, b: Int): Stream[Int] = a #:: fibFrom(b, a + b)
+    fibFrom(0, 1)(n-1)
+  }
+
+  // Scan left stream solution, requires 2.11
+  //def fib4(n: Int): Int = {
+  //  require(n >= 1, "Invalid n")
+  //  val str: Stream[Int] = 0 #:: str.scanLeft(1)(_ + _)
+  //  str(n-1)
+  //}
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
@@ -129,7 +155,18 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    val len = as.length
+    @annotation.tailrec
+    def isSortedInner(idx: Int, prev: A): Boolean = {
+      if (idx == len) true
+      else if (gt(as(idx), prev)) isSortedInner(idx + 1, as(idx))
+      else false
+    }
+
+    if (len == 0) true // well, sorta defined
+    else isSortedInner(1, as(0))
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
@@ -142,13 +179,13 @@ object PolymorphicFunctions {
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    (a: A) => partial1(a, f)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    (a: A, b: B) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -163,5 +200,5 @@ object PolymorphicFunctions {
   // Exercise 5: Implement `compose`
 
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    a => f(g(a))
 }
